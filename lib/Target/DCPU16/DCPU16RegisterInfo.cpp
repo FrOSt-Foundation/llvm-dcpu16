839342d5e06cd055a0ec150829026c9106905d79
L3nn0x <dragon.super@laposte.net>
2017-02-16 15:03:29 +0100

    Use Z for third argument

--- lib/Target/DCPU16/DCPU16RegisterInfo.cpp
+++ lib/Target/DCPU16/DCPU16RegisterInfo.cpp
@@ -41,12 +41,12 @@ DCPU16RegisterInfo::getCalleeSavedRegs(const MachineFunction *MF) const {
   const Function* F = MF->getFunction();
   static const MCPhysReg CalleeSavedRegs[] = {
     DCPU16::FP, DCPU16::R1A, DCPU16::R2B, DCPU16::R3C,
-    DCPU16::R6Z, DCPU16::R7I,
+    DCPU16::R7I,
     0
   };
   static const MCPhysReg CalleeSavedRegsFP[] = {
     DCPU16::R1A, DCPU16::R2B, DCPU16::R3C,
-    DCPU16::R6Z, DCPU16::R7I,
+    DCPU16::R7I,
     0
   };
   static const MCPhysReg CalleeSavedRegsIntr[] = {
